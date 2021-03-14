519d615c31cb35714bf1d6ec0d3a6ef0ad93da27
Abhijeet Tiwari <79205704+developerabhijeet@users.noreply.github.com>
2021-03-14 13:29:08 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -14,46 +14,45 @@ const Home = () => {
 				</div>
 				<ul className="collection list with-header">
 					<li className="collection-header">
-						<h4>Features of Applicaiton</h4>
+					<h4>Features of Applicaiton</h4>
 					</li>
 					<li className="collection-item">
-						<div>1. You can Share Your Issues/ Errors on Portal.
-      <a href="#!" className="secondary-content">
-								<i className="material-icons">send</i>
-							</a>
-						</div>
+					<div>1. You can Share Your Issues/ Errors on Portal.
+      						<a href="#!" className="secondary-content">
+							<i className="material-icons">send</i>
+						</a>
+					</div>
 					</li>
 					<li className="collection-item">
-						<div>2. You can Reply to other's Errors.
-     <a href="#!" className="secondary-content">
-								<i className="material-icons">send</i>
-							</a>
-						</div>
+					<div>2. You can Reply to other's Errors.
+     						<a href="#!" className="secondary-content">
+							<i className="material-icons">send</i>
+						</a>
+					</div>
 					</li>
 					<li className="collection-item">
-						<div>3. Give solutions in your own way.
-    <a href="#!" className="secondary-content">
-								<i className="material-icons">send</i>
-							</a>
-						</div>
+					<div>3. Give solutions in your own way.
+    						<a href="#!" className="secondary-content">
+							<i className="material-icons">send</i>
+						</a>
+					</div>
 					</li>
 					<li className="collection-item">
-						<div>4. Can see the Issues/Errors Posted Earlier for Solution Reference
-    <a href="#!" className="secondary-content">
-								<i className="material-icons">send</i>
-							</a>
-						</div>
+					<div>4. Can see the Issues/Errors Posted Earlier for Solution Reference
+    						<a href="#!" className="secondary-content">
+							<i className="material-icons">send</i>
+						</a>
+					</div>
 					</li>
 
 					<li className="collection-item">
-						<div>5. Get the instant reply for seniors/mentors during WFH.
-    <a href="#!" className="secondary-content">
-								<i className="material-icons">send</i>
-							</a>
-						</div>
+					<div>5. Get the instant reply for seniors/mentors during WFH.
+    						<a href="#!" className="secondary-content">
+							<i className="material-icons">send</i>
+						</a>
+					</div>
 					</li>
 				</ul>
-
 			</div>
 		</div>
 
